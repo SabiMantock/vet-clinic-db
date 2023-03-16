a3b7c594e11327f472a350bc729024d73fabc750
SabiMantock <sabimantock@gmail.com>
2023-03-16 12:45:50 +0000

    created patients, medical_histories and invoices

---
+++ schema_based_on_diagram.sql
@@ -0,0 +1,21 @@
+CREATE DATABASE clinic;
+CREATE TABLE patients(
+    id BIGSERIAL NOT NULL,
+    name VARCHAR(100) NOT NULL,
+    date_of_birth DATE NOT NULL,
+    PRIMARY KEY(id)
+);
+CREATE TABLE medical_histories(
+    id BIGSERIAL NOT NULL,
+    admitted_at TIMESTAMP NOT NULL,
+    patient_id INT REFERENCES patients(id),
+    status VARCHAR(100)
+);
+CREATE TABLE invoices (
+    id BIGSERIAL NOT NULL,
+    total_amount DECIMAL NOT NULL,
+    generated_at TIMESTAMP,
+    payed_at TIMESTAMP,
+    medical_history_id INT REFERENCES medical_history(id),
+    PRIMARY KEY(id)
+);
