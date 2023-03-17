adc9227c6de8aabf71bfb500f34a4eed3682e789
SabiMantock <sabimantock@gmail.com>
2023-03-17 07:43:57 +0000

    added FK indexes

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -9,7 +9,8 @@ CREATE TABLE medical_histories(
     id BIGSERIAL NOT NULL,
     admitted_at TIMESTAMP NOT NULL,
     patient_id INT REFERENCES patients(id),
-    status VARCHAR(100)
+    status VARCHAR(100) PRIMARY KEY (id),
+    INDEX (patient_id)
 );
 CREATE TABLE invoices (
     id BIGSERIAL NOT NULL,
@@ -17,19 +18,21 @@ CREATE TABLE invoices (
     generated_at TIMESTAMP,
     payed_at TIMESTAMP,
     medical_history_id INT REFERENCES medical_history(id),
-    PRIMARY KEY(id)
+    PRIMARY KEY(id),
+    INDEX (medical_history_id)
 );
 CREATE TABLE treatments (
     id BIGSERIAL NOT NULL,
     type VARCHAR(100) NOT NULL,
     name VARCHAR(100) NOT NULL,
     REFERENCES medical_history(id),
-    PRIMARY KEY(id)
+    PRIMARY KEY(id),
 );
 CREATE TABLE invoice_items(
     id BIGSERIAL NOT NULL,
     unit_price decimal NOT NULL,
     Quantity int,
     invoice_id int REFERENCES invoice (id),
-    treatment_id int REFERENCES treatments (id)
+    treatment_id int REFERENCES treatments (id),
+    INDEX (invoice_id, treatment_id)
 );
